209503872722f7a5ba4cdf9ea3cffa7ba146a0d5
Matias Umanzor <m.umanzorbriones@gmail.com>
2022-11-12 22:36:36 -0600

    Nuevos archivos a utilizar

---
+++ store/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+
+#<!-- 
+#    Main.html  → Plantilla de la que todos heredarán
+#    Store.html  → Página de inicio/fachada de la tienda con todos los productos
+#    Cart.html  → Carrito de compras de los usuarios
+#    Checkout.html  → Página de pago 
+#-->
+
+# recuerda que main no se referencia porque es la plantilla base para todas 
+urlpatterns = [
+    #dejar vacia la url base
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout")
+]
