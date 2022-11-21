e2a50bf4dbb3b941d01fbf60c5d8ba93d950c8d3
Matias Umanzor <m.umanzorbriones@gmail.com>
2022-11-20 21:15:24 -0600

    Se trabajo creando modelo DB

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Aqui registramos nuestros modelos creados 
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
