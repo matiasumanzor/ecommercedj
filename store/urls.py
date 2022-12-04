bcdcb35b9b89d1bd85f59360fd94eb63c067c5f2
Matias Umanzor <m.umanzorbriones@gmail.com>
2022-12-04 13:48:35 -0600

    Checkout completo favor de no tcar

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 #<!-- 
 #    Main.html  → Plantilla de la que todos heredarán
 #    Store.html  → Página de inicio/fachada de la tienda con todos los productos
@@ -10,6 +9,8 @@ from . import views
 #-->
 
 # recuerda que main no se referencia porque es la plantilla base para todas 
+
+
 urlpatterns = [
     #dejar vacia la url base
     path('', views.store, name="store"),
