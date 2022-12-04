235563c9a4d6ab020750c53f06ebdc0455beca9e
Matias Umanzor <m.umanzorbriones@gmail.com>
2022-12-04 12:54:26 -0600

    Carrito de compras terminado favor de no tocar

--- store/models.py
+++ store/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Creacion  modelo tablas
 
+
+# Tabla clientes:
+#  user  = nombre de usuario
+#  name  = Nombre real 
+#  email = Direccion de correo
 class Customer(models.Model):
     user             =  models.OneToOneField  (User, on_delete=models.CASCADE, null=True, blank=True)
     name             =  models.CharField      (max_length=200, null=True)
@@ -10,10 +16,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+# Tabla productos
+# name = Nombre producto
+# price = Precio
+# image = Imangen (se agrega desde dentro del administrador)
 class Product(models.Model):
     name             =  models.CharField       (max_length=200, null=True)
     price            =  models.FloatField      ()
-    digital          =  models.BooleanField    (default=False,  null=True, blank=False)
     image            =  models.ImageField      (null=True, blank=True)
 
     def __str__(self):
@@ -28,7 +37,6 @@ class Product(models.Model):
             url = ''
         return url
 
-
 class Order(models.Model):
     customer         =  models.ForeignKey      (Customer, on_delete=models.SET_NULL, blank=True, null=True)
     date_orderd      =  models.DateTimeField   (auto_now_add=True)
@@ -38,12 +46,50 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+
+    #Este fragmento se utiliza para el calculo dentro del carrito de compras
+    #Captura las variables deseadas para posterior ser utilizadas para definir el total de la compra
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.qunatity for item in orderitems])
+        return total
+
+
+# Tabla de pedidos
+#   product    = Nombre producto
+#   order      = N° orden
+#   qunatity   = Cantidad
+#   date_added = Fecha
 class OrderItem(models.Model):
     product          =  models.ForeignKey      (Product, on_delete=models.SET_NULL, blank=True, null=True)
     order            =  models.ForeignKey      (Order, on_delete=models.SET_NULL, blank=True, null=True)
     qunatity         =  models.IntegerField    (default=0, null=True, blank=True)
     date_added       =  models.DateTimeField   (auto_now_add=True)
 
+
+    #Calculo precio lote de productos agregados al carrito
+    #Toma el precio del producto y lo multiplica por la cantidad deseada
+    @property
+    def get_total(self):
+        total = self.product.price * self.qunatity
+        return total
+
+
+# Tabla de direccion comprador
+#   customer   = Cliente 
+#   order      = N° de orden
+#   address    = Direccion comprador 
+#   city       = Ciudad comprador
+#   state      = Comuna 
+#   zipcode    = Codigo postal
+#   date_added = Fecha
 class ShippingAddress(models.Model):
     customer         =  models.ForeignKey      (Customer, on_delete=models.SET_NULL,blank=True, null=True)
     order            =  models.ForeignKey      (Order, on_delete=models.SET_NULL, blank=True, null=True)
